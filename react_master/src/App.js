b81bcbfbafc4451b52fee73ca119d38205b25671
최원서 <dnjstj88@naver.com>
2023-11-06 18:59:22 +0900

    styledComponents 정리

--- react_master/src/App.js
+++ react_master/src/App.js
@@ -1,21 +1,47 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 const Father = styled.div`
   display: flex;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 const Btn = styled.button`
   color: white;
-  background-color: tomato;
   border-radius: 15px;
   border: 0;
 `;
-
+const Emoji = styled.span`
+  font-size: 40px;
+`;
+const rotationanimation = keyframes`
+0%{
+  transform: rotate(0deg);
+  border-radius: 0px;
+}50%{
+  
+  border-radius: 100px;
+}100%{
+  transform: rotate(360deg);
+  border-radius: 100px;
+}
+`;
 const Box = styled.div`
   //props 통한 컴포넌트 설정
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+  background-color: ${(props) => props.theme.textColor};
+  width: 200px;
+  height: 200px;
+  display: flex;
+  animation: ${rotationanimation} 3s linear infinite;
+  justify-content: center;
+  align-items: center;
+  ${Emoji} {
+    &:hover {
+      font-size: 70px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
 // const BoxTwo = styled.div`
 //   background-color: tomato;
@@ -34,20 +60,32 @@ const Input = styled.input.attrs({ required: true, maxLength: 5 })`
   //속성값을 설정해줄 수 있음.
   background-color: tomato;
 `;
+const Wrapper = styled.div`
+  background-color: ${(props) => props.theme.backgroundColor};
+  width: 100vw;
+  height: 100vh;
+`;
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Btn> Log in</Btn>
-      <Btn as="a" href="/">
-        Log in A
-      </Btn>
-      {/* 태그는 a로 바뀐다 */}
-      <Circle bgColor="tomato" />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Father>
+        <Box bgColor="teal" />
+        <Btn> Log in</Btn>
+        <Btn as="a" href="/">
+          Log in A
+        </Btn>
+        {/* 태그는 a로 바뀐다 */}
+        <Circle bgColor="tomato" />
+        <Input />
+        <Input />
+        <Input />
+
+        <Box>
+          <Emoji as="p">🥰</Emoji>
+        </Box>
+        <Emoji>😍</Emoji>
+      </Father>
+    </Wrapper>
   );
 }
 
